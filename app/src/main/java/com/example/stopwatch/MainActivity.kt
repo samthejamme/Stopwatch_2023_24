5012140295e9806e7f348e720ff015de4f4dc7b9
Sam <sjwong723@gmail.com>
2023-10-17 10:04:54 -0700

    10.17.23

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -6,25 +6,20 @@ import android.os.SystemClock
 import android.util.Log
 import android.widget.Button
 import android.widget.Chronometer
-import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private lateinit var startButton: Button
     private lateinit var stopButton: Button
     private lateinit var timer: Chronometer
     private var started: Boolean = false; private var paused: Boolean = false; private var stopped: Boolean = true
-    private var displayTime: Long = 0; private var stoppedTime: Long = 0
+    private var stoppedTime: Long = 0
     companion object {
         // TAG is the default for naming logs
         val TAG = "MainActivity"
-        // this is to test github pushing
-        val ASTROPHYSICISTS_PI = 3
 
         // make constants for you key-value pairs
-        val STATE_DISPLAY_TIME = ""
-        val IS_STATE_RUNNING = ""
-        val IS_STATE_PAUSED = ""
-        val IS_STATE_STOOPED = ""
+        val STATE_DISPLAY_TIME = "actual time"
+        val IS_STATE_RUNNING = "it's running"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,72 +29,69 @@ class MainActivity : AppCompatActivity() {
 
         startButton = findViewById(R.id.button_main_startbutton)
         stopButton = findViewById(R.id.button2_main_stopButton)
-//        timer = findViewById(R.id.chronometer_main_stopwatch)
 
         wireWidgets()
 
         // restore saveInstanceState if it exists
         if(savedInstanceState != null) {
-            displayTime = savedInstanceState.getLong(STATE_DISPLAY_TIME)
+            stoppedTime = savedInstanceState.getLong(STATE_DISPLAY_TIME)
             started = savedInstanceState.getBoolean(IS_STATE_RUNNING)
-            timer.base = SystemClock.elapsedRealtime() - displayTime
-            timer.start()
-
-            started = true; stopped = false; paused = false
+            timer.base = SystemClock.elapsedRealtime() - stoppedTime
+            if(started) {
+                timer.start()
+            }
         }
 
         startButton.setOnClickListener() {
-            timer.start()
-            timer.base = SystemClock.elapsedRealtime() + stoppedTime
-            if(stopped)
+            if(!started)
             { // if it's BEEN stopped
-                itsStarted()
-                startButton.text = "Resume"
-                stopButton.text = "Pause" // include this when it gets started again from rotating
-            }
-            else if(paused) {
-                // if it's BEEN paused
-                itsStarted()
+                timer.start()
+                timer.base = SystemClock.elapsedRealtime() - stoppedTime
+                started = true
                 startButton.text = "Resume"
                 stopButton.text = "Pause"
             }
         }
 
         stopButton.setOnClickListener() {
-            timer.stop()
-            stoppedTime = timer.base - SystemClock.elapsedRealtime()
             if(started) {
+                timer.stop()
                 // if it's BEEN started
-                itsPaused()
+                started = false
+                stoppedTime = SystemClock.elapsedRealtime() - timer.base
                 startButton.text = "Resume"
                 stopButton.text = "Stop"
             }
-            else if(paused) {
-                // if it's BEEN paused
-                itsStopped()
-                startButton.text = "Start"
-                stopButton.text = "Stop"
-                timer.base = SystemClock.elapsedRealtime()
+            else {
+                reset()
             }
         }
+    }
+
+    private fun reset() {
+        started = false
+        startButton.text = "Start"
+        stopButton.text = "Stop"
+        timer.base = SystemClock.elapsedRealtime()
+        stoppedTime = 0
     }
 
     private fun wireWidgets() {
         timer = findViewById(R.id.chronometer_main_stopwatch)
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
         // calculate the display time if currently running
         if(started) {
-            displayTime = timer.base - SystemClock.elapsedRealtime()
+            stoppedTime = SystemClock.elapsedRealtime() - timer.base
         }
         // save key-value pairs to the bundle before the superclass call
-        outState.putLong(STATE_DISPLAY_TIME, displayTime)
-        outState.putBoolean(IS_STATE_RUNNING, started)
-        outState.putBoolean(IS_STATE_PAUSED, paused)
-        outState.putBoolean(IS_STATE_STOOPED, stopped)
+        savedInstanceState.putLong(STATE_DISPLAY_TIME, stoppedTime)
+        savedInstanceState.putBoolean(IS_STATE_RUNNING, started)
+//        savedInstanceState.putBoolean(IS_STATE_PAUSED, paused)
+//        savedInstanceState.putBoolean(IS_STATE_STOOPED, stopped)
 
-        super.onSaveInstanceState(outState)
+        super.onSaveInstanceState(savedInstanceState)
     }
 
 
@@ -107,32 +99,24 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
-
-
-    // switching the booleans
-    fun switchNames() {
-
-    }
-
-    fun itsStarted() {
-        started = true
-        stopped = false
-        paused = false
-    }
-
-    fun itsPaused() {
-        started = false
-        stopped = false
-        paused = true
-    }
-
-    fun itsStopped() {
-         started = false
-        stopped = true
-        paused = false
-    }
+//
+//    fun itsStarted() {
+//        started = true
+//        stopped = false
+//        paused = false
+//    }
+//
+//    fun itsPaused() {
+//        started = false
+//        stopped = false
+//        paused = true
+//    }
+//
+//    fun itsStopped() {
+//         started = false
+//        stopped = true
+//        paused = false
+//    }
 
 
 
